19a7c895fb0d2c6296b6add0d2befafc054ba162
nithin.prakash <nithin.prakash@qburst.com>
2022-12-26 17:23:42 +0530

    created sign in page and sign Up page

---
+++ src/views/signin/SignIn.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, onMounted } from "vue";
+</script>
+<template>
+  <section id="sign_in">
+    <div id="sign_in_image"></div>
+    <div id="sign_in_content">
+      <form>
+        <h1>Welcome Back</h1>
+        <label>User Name <input type="text" placeholder="User Name" /></label>
+        <label>Password<input type="password" placeholder="Password" /></label>
+        <a id="sign_in_anchor" href="#">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Sign In
+        </a>
+        <p>
+          Dont have an account ?
+          <RouterLink :to="{ name: 'SignUp' }"><a>Sign Up</a></RouterLink>
+        </p>
+      </form>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+    @import "./sign-in.css";
+</style>
---
+++ src/views/signup/SignUp.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, onMounted } from "vue";
+</script>
+<template>
+  <section id="sign_up">
+    <div id="sign_up_image"></div>
+    <div id="sign_up_content">
+      <form action="">
+        <h1>Lets Go Shopping</h1>
+        <label>Full Name <input type="text" placeholder="Name" /></label>
+        <label>Email <input type="email" placeholder="Email" /></label>
+        <label>Phone Number<input type="text" placeholder="Number" /></label>
+        <label>Password<input type="password" placeholder="Password" /></label>
+        <label>Confirm Password<input type="password" placeholder="Password" /></label>
+        <div id="button_div">
+          <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Sign Up
+          </a>
+          
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+<style scoped>
+@import "./sign-up.css";
+</style>
