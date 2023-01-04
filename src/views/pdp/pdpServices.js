f15db98ea2576ee03cf18e930f391f9d53470f0a
nithin.prakash <nithin.prakash@qburst.com>
2023-01-04 11:38:09 +0530

    proper images for fashion added

--- src/views/pdp/pdpServices.js
+++ src/views/pdp/pdpServices.js
@@ -3,6 +3,7 @@ import state from "./pdpState.js";
 
 export const getProducts = async (parameter) => {
     const response = await axios("https://rachelhepsan.github.io/ProductDetails/data.json")
+    // const response = await axios("../../src/assets/data.json")
     response.data.forEach(element => {
         if(+element.id === +parameter){
             state.results=element
